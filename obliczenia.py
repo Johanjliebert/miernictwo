73bc5f44c1032e9d7ffc45e99b947916658e2379
Johanjliebert <75436891+Johanjliebert@users.noreply.github.com>
2024-03-26 06:45:16 +0100

    Add files via upload

--- obliczenia.py
+++ obliczenia.py
@@ -34,18 +34,19 @@ def print_hi():
     # Use a breakpoint in the code line below to debug your script.
     print("0 - koniec programu")
     print("1 - odchylenie standardowe+srednie")
-    print("2 - rezystancja calkowita")
-    print("3 - niepewnosc obliczen")
+    print("2 - niepewnosc calkowita")
+    print("3 - srednia wazona")
+    print("4 - niepewnosc obliczen")
     co=input("Co chcesz obliczyc?")
     #while(co!=0):
     for z in range(1):
         suma=0
         ilosc=0
         match co:
-            case 1:
+            case '1':
                 srednia=0
                 print("wczytywanie wartosci pomiarow...")
-                pomiary=wczytaj_plik("wartosci.txt")
+                pomiary=wczytaj_plik("wartosci1.txt")
                 for pomiar in pomiary:
                     srednia+=pomiar
                     ilosc+=1
@@ -57,29 +58,75 @@ def print_hi():
 
                 odchylsr=odchyl/math.sqrt(ilosc)
 
-                zapisz=open("dane.txt",mode='w')
-                #zapisz.write("srednia = ")
+                zapisz=open("dane1.txt",mode='w')
                 zapisz.write(str(srednia)+"\n")
-                #zapisz.write("odchylenie standardowe = ")
                 zapisz.write(str(odchyl)+"\n")
-                #zapisz.write("odchylenie srednie = ")
                 zapisz.write(str(odchylsr)+"\n")
                 zapisz.close()
-            case 2:
-                dane=wczytaj_plik("dane.txt")
-                rizz=wczytaj_plik("rezystancja.txt")
-                rez=open("rezystancja_calkowita.txt",mode='w')
-                rez.write(str(math.sqrt(dane[1]*dane[1]+rizz[0]*rizz[0]/3+rizz[1]*rizz[1]/3)))
+
+                srednia=0
+                ilosc=0
+                print("wczytywanie wartosci pomiarow...")
+                pomiary=wczytaj_plik("wartosci2.txt")
+                for pomiar in pomiary:
+                    srednia+=pomiar
+                    ilosc+=1
+                srednia/=ilosc
+                for wartosc in pomiary:
+                    suma+=(wartosc-srednia)*(wartosc-srednia)
+                odchyl=math.sqrt(suma/(ilosc-1))
+
+
+                odchylsr=odchyl/math.sqrt(ilosc)
+
+                zapisz=open("dane2.txt",mode='w')
+                zapisz.write(str(srednia)+"\n")
+                zapisz.write(str(odchyl)+"\n")
+                zapisz.write(str(odchylsr)+"\n")
+                zapisz.close()
+            case '2':
+                dane=wczytaj_plik("dane1.txt")
+                rizz=wczytaj_plik("rezystancja1.txt")
+                np=open("niepewnosc_calkowita1.txt",mode='w')
+                np.write(str(math.sqrt(dane[1]*dane[1]+rizz[0]*rizz[0]/3+rizz[1]*rizz[1]/3)))
+                np.close()
+                dane=wczytaj_plik("dane2.txt")
+                rizz=wczytaj_plik("rezystancja2.txt")
+                np=open("niepewnosc_calkowita2.txt",mode='w')
+                np.write(str(math.sqrt(dane[1]*dane[1]+rizz[0]*rizz[0]/3+rizz[1]*rizz[1]/3)))
+                np.close()
             case '3':
+                waga=list()
+                listsrednia=list()
+                listsrednia.append(wczytaj_plik("dane1.txt")[0])
+                listsrednia.append(wczytaj_plik("dane2.txt")[0])
+                waga.append(wczytaj_plik("niepewnosc_calkowita1.txt")[0])
+                waga.append(wczytaj_plik("niepewnosc_calkowita2.txt")[0])
+                wait=open("waga.txt", mode='w')
+                waga[0]=1/(waga[0]*waga[0])
+                waga[1]=1/(waga[1]*waga[1])
+                wait.write(str(waga[0])+"\n")
+                wait.write(str(waga[1])+"\n")
+                wait.write(str((waga[0]*listsrednia[0]+waga[1]*listsrednia[1])/(waga[0]+waga[1]))+"\n")
+                wait.write(str(math.sqrt(1/(waga[0]+waga[1]))))
+            case '4':
                 wartosci=wczytaj_plik("wartosci.txt")
                 niepewnosc = open("niepewnosc.txt",mode='w')
                 for wartosc in wartosci:
-                    x=0.01
-                    if(wartosc<2):
-                        x=0.001
-                    if(wartosc<0.2):
-                        x=0.00001
-                    niepewnosc.write(str(wartosc/1000+x)+"\n")
+                    suma=0
+                    wart=str(wartosc)
+                    if(wartosc>10000):
+                        suma+=int(wart[0])*10
+                        suma+=int(wart[1])*1
+                        suma+=int(wart[2])*0.5
+                        suma+=int(wart[3])*0.01
+                        suma+=int(wart[4])*0.002
+                    else:
+                        suma+=int(wart[0])*1
+                        suma+=int(wart[1])*0.5
+                        suma+=int(wart[2])*0.01
+                        suma+=int(wart[3])*0.002
+                    niepewnosc.write(str(suma)+"\n")
                 niepewnosc.close()
         #co = input("Co chcesz obliczyc?")
 
